6ec5c1577a2489ae2d4bb0ae80e1ace8d32d2078
Valery Bugakov <skymk1@gmail.com>
2018-08-21 16:27:54 +0300

    Mobile: removed redundant Splash component

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,13 +2,11 @@ import { createSwitchNavigator } from 'react-navigation';
 
 import wrapNavigator from 'components/shared/wrapNavigator';
 
-import Splash from 'components/Splash';
 import AuthStack from 'components/Auth';
 import AppStack from 'components/App';
 
 const MainNavigator = createSwitchNavigator(
   {
-    Splash,
     Auth: AuthStack,
     App: AppStack,
   },
--- src/components/Splash/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.View``;
-
-export const Title = styled.Text``;
