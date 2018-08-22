e9feffb36dbdc8c56d9e46627f03a591d4d7c909
Sergey Nebolsin <sergey@nebols.in>
2018-08-22 04:21:34 +0300

    ios: integrate Fabric/Crashlytics

--- ios/MobiusMobileWallet/AppDelegate.m
+++ ios/MobiusMobileWallet/AppDelegate.m
@@ -6,6 +6,8 @@
  */
 
 #import "AppDelegate.h"
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -14,6 +16,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [Fabric with:@[[Crashlytics class]]];
+
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
