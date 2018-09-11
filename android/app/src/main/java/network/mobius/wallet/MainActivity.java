c167df656c1a5b2651ad9345d411d38cb5731673
Sergey Nebolsin <sergey@nebols.in>
2018-09-11 05:09:40 +0300

    android: implement proper splash screen

--- android/app/src/main/java/network/mobius/wallet/MainActivity.java
+++ android/app/src/main/java/network/mobius/wallet/MainActivity.java
@@ -1,6 +1,10 @@
 package network.mobius.wallet;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
+
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "MobiusMobileWallet";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
     }
 }
