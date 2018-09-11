0f4d4d6e7188afaa1c327968fae322b06dfc1e94
Emil Kashkevich <emil.kashkevich@gmail.com>
2018-09-11 03:27:17 +0200

    [MOB-453] Add loading screen for login process

--- src/state/sagas/auth/index.js
+++ src/state/sagas/auth/index.js
@@ -1,3 +1,3 @@
-export { default as loginSaga } from './loginSaga';
+export { default as loginStartSaga } from './loginStartSaga';
 export { default as signupFinishSaga } from './signupFinishSaga';
 export { default as signupStartSaga } from './signupStartSaga';
