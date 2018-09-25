a45150fabdbcdc91093fe23d0c1df99cf1f317d0
Gleb Pospelov <codefo@codefo.com>
2018-09-25 17:57:20 +0300

    [MOB-484] Alphabetical sort

--- android/app/src/main/java/network/mobius/wallet/MainApplication.java
+++ android/app/src/main/java/network/mobius/wallet/MainApplication.java
@@ -6,12 +6,12 @@ import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
 import com.facebook.react.ReactApplication;
-import com.apsl.versionnumber.RNVersionNumberPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.apsl.versionnumber.RNVersionNumberPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.horcrux.svg.SvgPackage;
@@ -39,7 +39,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNVersionNumberPackage(),
             new FabricPackage(),
             new KeychainPackage(),
             new LinearGradientPackage(),
@@ -47,6 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
             new RNCameraPackage(),
             new RNDeviceInfo(),
             new RNLanguagesPackage(),
+            new RNVersionNumberPackage(),
             new SplashScreenReactPackage(),
             new SvgPackage(),
             new VectorIconsPackage()
