aea782120cca0ec58f8580cf33ea5cb5672f5665
Mateus Zitelli <zitellimateus@gmail.com>
2018-10-22 10:04:43 -0700

    Remove unused props mapper

--- src/components/App/Currencies/CurrenciesContainer.js
+++ src/components/App/Currencies/CurrenciesContainer.js
@@ -1,12 +1,9 @@
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
-import { createStructuredSelector } from 'reselect';
 
 import { userCurrenciesActions } from 'state/userCurrencies';
 import Currencies from './Currencies';
-
-const mapStateToProps = createStructuredSelector({});
 
 const actions = {
   ...userCurrenciesActions,
@@ -14,7 +11,7 @@ const actions = {
 
 export default compose(
   connect(
-    mapStateToProps,
+    null,
     actions
   ),
   translate('translation')
