727dc1dd77f5ffaea2ccda521b86f5cf971ceb0d
Mateus Zitelli <zitellimateus@gmail.com>
2018-10-23 13:07:45 -0700

    Sort currencies by market cap

--- src/components/App/Currencies/CurrenciesList/CurrenciesListContainer.js
+++ src/components/App/Currencies/CurrenciesList/CurrenciesListContainer.js
@@ -3,7 +3,11 @@ import { connect } from 'react-redux';
 import CurrenciesList from './CurrenciesList';
 
 const mapStateToProps = state => ({
-  currencies: Object.keys(state.currencies).map(key => state.currencies[key]),
+  currencies: Object.keys(state.currencies)
+    .map(key => state.currencies[key])
+    .sort(
+      (currencyA, currencyB) => (currencyA.quote.USD.market_cap > currencyB.quote.USD.market_cap ? -1 : 1)
+    ),
 });
 
 export default connect(mapStateToProps)(CurrenciesList);
